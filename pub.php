9ff7455bc3d5f8864d020f0ae52f35ea65ef8911
Chaiyakorn  Khunthong <bas31234@hotmail.com>
2018-12-15 19:23:49 +0700

    Update pub.php
    change name in funtion pubmqtt to APPLICATION KEY on app id botlinesmartwater
    and set key and Secret

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot/"; //enter your appid
-     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
-    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+       $APPID= "botlinesmartwater"; //enter your appid
+     $KEY = "OwZTrkVHgCSj8Cb"; //enter your key
+    $SECRET = "RW5U2uVafq27gW1Mm7kpUMNaB"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
